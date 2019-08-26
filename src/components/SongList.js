d91411eda7de08cfb6a05db62a3309fb436c901c
Denis John <denis.john.ndarake@gmail.com>
2019-08-26 21:00:50 +0100

    Cleaned the app and created a new SongList Component

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SongList = () => {
+  return (
+    <div>
+      <ul>
+        <li>Hello Song</li>
+        <li>Queen Song</li>
+        <li>It's in me</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
