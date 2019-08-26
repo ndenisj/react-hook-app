cc99b722a9586231bfa13547ec96ba1c81b2e1d5
Denis John <denis.john.ndarake@gmail.com>
2019-08-26 21:40:01 +0100

    Imported useState and created state inside a function base component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import uuid from "uuid/v1";
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: "My song 4 you", id: 1 },
+    { title: "My song 4 you 2", id: 2 },
+    { title: "My song 4 you 3", id: 3 }
+  ]);
+  const addSong = () => {
+    setSongs([...songs, { title: "My new song", id: uuid() }]);
+  };
   return (
     <div>
       <ul>
-        <li>Hello Song</li>
-        <li>Queen Song</li>
-        <li>It's in me</li>
+        {songs.map(song => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
       </ul>
+      <button onClick={addSong}>Add song</button>
     </div>
   );
 };
